90a565445c1c46c1a81aff07ac07dff3e9c3d8ca
MuzamilSyed <muzamilshah806@gmail.com>
2024-06-09 13:23:56 +0500

    extra removed

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" , "Scorching2" , "Scorching3" , "abc" , "shah"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" 
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
