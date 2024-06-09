332ddf6fed2ccebb2a0c4ea2c22aeafec6914a8d
MuzamilSyed <muzamilshah806@gmail.com>
2024-06-09 13:20:42 +0500

    Changes in string

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" , "Scorching2" , "Scorching3"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" , "Scorching2" , "Scorching3" , "abc"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
