b2d2fd848ae1388e2c13398f6430988f89bb549e
MuzamilSyed <muzamilshah806@gmail.com>
2024-06-10 12:59:14 +0500

    added new controller

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Content("Hello World", "text/plain");
+        }
+    }
+}
